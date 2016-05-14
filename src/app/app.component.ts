d32e5c87cdf0092909f02d86320922a838ac08ce
Aaron Heinrich <Aaron Heinrich>
2016-05-14 15:46:26 -0700

    - using md components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Routes, Router, ROUTER_DIRECTIVES } from '@angular/router';
+import { MdButton } from '@angular2-material/button';
+import { MD_LIST_DIRECTIVES } from '@angular2-material/list'
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card'
+import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav'
+import { MdToolbar } from '@angular2-material/toolbar';
+
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
 
 @Component({
     moduleId: module.id,
     selector: 'app',
     templateUrl: 'app.component.html',
-    directives: [ROUTER_DIRECTIVES],
+    styleUrls: ['app.component.css'],
+    directives: [
+        ROUTER_DIRECTIVES, 
+        MD_SIDENAV_DIRECTIVES,
+        MD_LIST_DIRECTIVES, 
+        MD_CARD_DIRECTIVES,
+        MdIcon, 
+        MdToolbar,
+         MdButton],
+    providers: [ MdIconRegistry ]
 })
 @Routes([
 ])
